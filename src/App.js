b3aef19885262828625db14a457cbb055f233d06
Isabel <isabel.paulo.mineiro@gmail.com>
2024-05-23 22:50:15 +0200

    Continuação de Estilização : Sass Instalado e Dark/Light mode escrito no sass

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <h1>Previsão 360</h1>
+      <p>Meteorologia intuitiva para todos</p>
+      {/* i class vem qweather.com */}
+      <i class="qi-300-fill"></i>
     </div>
   );
 }
