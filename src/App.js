051c30e5cca680bc63b7808edb4b5924c1afc764
Isabel <isabel.paulo.mineiro@gmail.com>
2024-06-03 18:39:48 +0200

    Incio de NavBar feito : Pagina Inicial e Mapa Infravermelho

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import DadosMeteo from './DadosMeteo.js';
+import NavBar from './NavBar.js';
 
 function App() {
   return (
     <div className="container">
       <h1>Previsão 360</h1>
+      <NavBar />
       <p>Meteorologia intuitiva para todos</p>
       <DadosMeteo />
       {/* i class vem qweather.com */}
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function Navbar() {
+  return (
+    <nav>
+      <button className="button">Página Inicial</button>
+      <button className="button">Mapa Infravermelho</button>
+    </nav>
+  );
+}
+
+export default Navbar;
